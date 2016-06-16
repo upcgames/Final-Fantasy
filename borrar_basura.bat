1d761b3b931a72fa72e2cb915243e3e467b0a413
Diego Jara <saintplay96@gmail.com>
2016-06-15 22:33:44 -0500

    Script para borrar basura añadido

---
+++ borrar_basura.bat
@@ -0,0 +1,15 @@
+
+erase /F /S /Q *.ncb
+erase /F /S /Q *.exe
+erase /F /S /Q *.ilk
+erase /F /S /Q *.pdb
+erase /F /S /Q *.obj
+erase /F /S /Q *.htm
+erase /F /S /Q *.dep
+erase /F /S /Q *.manifest
+erase /F /S /Q *.pch
+erase /F /S /Q *.idb
+erase /F /S /Q *.pdb
+erase /F /S /Q *.ppt
+erase /F /S /Q *.sdf
+erase /F /S /Q *.ipch
